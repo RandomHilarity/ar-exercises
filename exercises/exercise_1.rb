fba396b2519099870354d48276ba8760bd1a5ae0
John Webster <johnrwebs@gmail.com>
2019-09-16 21:06:35 +0000

    exercise 1 completed

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,27 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+class Store < ActiveRecord::Base
+end
+
+Store.create do |s|
+    s.name = "Burnaby"
+    s.annual_revenue = 300000
+    s.mens_apparel = true
+    s.womens_apparel = true
+end
+
+Store.create do |s|
+    s.name = "Richmond"
+    s.annual_revenue = 1260000
+    s.womens_apparel = true
+end
+
+Store.create do |s|
+    s.name = "Gastown"
+    s.annual_revenue = 190000
+    s.mens_apparel = true
+end
+
+puts Store.count
